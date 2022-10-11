ef0df2d1b3ef594202931d95b71b9ce3e6d726f6
MathSSouza98 <88606648+MathSSouza98@users.noreply.github.com>
2022-10-11 19:47:33 -0300

    modified app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import Layout from './components/Layout';
-
 import Dashboard from './Pages/Dashboard';
-
 import GlobalStyles from './styles/GlobalStyles';
-
 import dark from './styles/themes/dark';
-
 import light from './styles/themes/light';
 
 const App: React.FC = () => {
